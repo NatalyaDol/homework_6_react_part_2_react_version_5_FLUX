530283e366b0455ac31f82e83989bbbc6f61eca8
NatalyaDol <suhoikova@gmail.com>
2019-06-27 13:20:21 +0300

    Add the function Add Comments

---
+++ src/app/pages/Posts.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { addPost, getPosts } from '../actions/postActions';
+import store from '../stores/postStore';
+import PostsList from '../components/PostsList';
+
+export default class Posts extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+
+     //Реакт создай ссылку  к которой я могу обратиться
+     this.myRefName = React.createRef();
+     this.myRefComment = React.createRef();
+     //console.log(this.myRefComment.current.value, this.myRefName.current.value);
+      
+   
+
+    this.onPostChange = this.onPostChange.bind(this);
+    this.newPost = this.newPost.bind(this);
+  }
+  
+  newPost() {
+    // const title = myRefName;
+    // const userId = 1;
+    // const body = myRefComment;
+    // addPost(title, userId, body);
+    const title = this.myRefName.current.value;
+    const userId = 1;
+    const body = this.myRefComment.current.value;
+
+    let newComment = {  title, userId, body};
+    const posts = [newComment, ...this.state.posts ];
+    console.log(posts);
+
+
+    //очистка введенных полей 
+    this.myRefName.current.value = '';
+    this.myRefComment.current.value = '';
+
+    //перерисовка
+    this.setState({ posts: posts });
+    console.log("newsetState");
+  }
+
+  onPostChange() {
+    this.setState({ posts: store.posts });
+  }
+
+  componentDidMount() {
+    getPosts();
+    store.on('change', this.onPostChange);
+  }
+
+  componentWillUnmount() {
+    store.removeListener('change', this.onPostChange);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Посты</h1>
+        
+        <label>
+            <input ref={this.myRefName} type="text" placeholder="Ваше имя"></input>
+            <br></br>
+            <textarea ref={ this.myRefComment } rows="4" cols="100" placeholder="Комментарий"></textarea>
+            <br></br>
+            <button onClick={this.newPost} className="btn btn-primary">Добавить пост</button>
+        </label>
+        <PostsList posts={this.state.posts} />
+      </div>
+    )
+  }
+}
